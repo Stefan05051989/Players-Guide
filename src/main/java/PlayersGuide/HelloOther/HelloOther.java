abc0ff496b9ebcc3f6d08167ae6fb65a928c6d4d
Stefan <stefan.kiers89@hotmail.com>
2025-06-23 16:32:43 +0200

    New Players guide

---
+++ src/main/java/PlayersGuide/HelloOther/HelloOther.java
@@ -0,0 +1,9 @@
+package PlayersGuide.HelloOther;
+
+
+
+public class HelloOther {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/PlayersGuide/HelloWorld/HelloWorld.java
@@ -0,0 +1,15 @@
+package PlayersGuide.HelloWorld;
+
+/*
+Stefan Kiers
+23-06-'25
+
+
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        String hello = "Hello Other World" + "\n";
+        System.out.println(hello.repeat (5 ));
+    }
+}
