7e3cc055b160724086ddd4e04f9588c217007200
Stefan <stefan.kiers89@hotmail.com>
2025-07-01 11:56:23 +0200

    players guide and class assignment programs.

---
+++ src/main/java/Com_PlayersGuide_KiersStefan/ClassAssignments/HelloWorld/HelloWorldClass.java
@@ -0,0 +1,15 @@
+package Com_PlayersGuide_KiersStefan.ClassAssignments.HelloWorld;
+
+/*
+Stefan Kiers
+23-06-'25
+
+ */
+
+public class HelloWorldClass {
+    public static void main(String[] args) {
+        String hello = "Hello Other World" + "\n";
+        System.out.print(hello.repeat (5 ));
+        System.out.println("Hello\nWorld"); // \n = new line inside statement.
+    }
+}
