51f4ed96e29ca005b18e881c251aad12a8dd1c4c
Stefan <stefan.kiers89@hotmail.com>
2025-07-20 17:06:18 +0200

    20 : method + call in other packages.

---
+++ src/main/java/Com_PlayersGuide_KiersStefan/Challenge_21/Countdown.java
@@ -0,0 +1,51 @@
+package Com_PlayersGuide_KiersStefan.Challenge_21;
+
+/*
+Stefan Kiers
+20-07-'25
+
+Objective : rewrite the following code, which counts down from 10 to 1, with no loops:
+for (int x = 10; x > 0; x--)
+ System.out.println(x);
+(recursion)
+ */
+public class Countdown {
+    public static void main(String[] args) {
+//        for (int x = 10; x > 0; x--)
+//            System.out.println(x);
+
+        baseForRecursion(3);
+        /*
+        Recursion is the technique of making the function call itself. You can break complicated problems down to
+        simpeler one that are easier to solve.
+        It's used to apply the result of a procedure, to a procedure.
+
+        Recursion = the repetition of a procedure.
+        Iteration = repetition of the process.
+         */
+
+    }
+    // psv int called base
+    public static void baseForRecursion(int base) { // 2 usages, 1 = inside class, send is the call in main.
+        // if base is greater than / equal to 0, return to recursive method call in main class (baseForRecursion)
+        if (base <= 0) {
+            return;
+        }
+        // print base that carries value of the recursive method at that time!
+        System.out.println(base);
+        // start with 10, base -1, base becomes 9, base -1, base becomes 8 etc.
+        // call the function again!
+        baseForRecursion(base - 1);
+
+        /*
+        summary :
+        baseFR int creates method for countdown.
+        method is executed until base hits zero (determined in if statement)
+        print statement for state of recursion
+        baseFR - 1
+        repeat function!
+        value is determined in main class as parameters (in this case, 10)
+
+         */
+    }
+}
