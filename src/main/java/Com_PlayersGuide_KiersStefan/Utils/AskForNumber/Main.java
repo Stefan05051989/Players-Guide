899fa91e72e9ba046e52e915aa0569ac404f1275
Stefan <stefan.kiers89@hotmail.com>
2025-07-21 20:33:12 +0200

    adjustments

--- src/main/java/Com_PlayersGuide_KiersStefan/Utils/AskForNumber/Main.java
+++ src/main/java/Com_PlayersGuide_KiersStefan/Utils/AskForNumber/Main.java
@@ -6,8 +6,9 @@ public class Main {
         // create local variabel to store input from user using method : int userInput
         // import method for asking number in range :
         // Import class by typing : NumberInRange, call method : .AskFor, give parameters (int) for method!
-        System.out.println("Choose input from range :  MIN.VALUE : -2147483648, MAX.VALUE : 2147483647 ");
-        int userInput = TakingANumber.AskForNumber(Integer.MIN_VALUE,  Integer.MAX_VALUE);
+        int userInput = TakingANumber.AskForNumber(
+                "Choose input from range :  MIN.VALUE : -2147483648, MAX.VALUE : 2147483647 ",
+                Integer.MIN_VALUE, Integer.MAX_VALUE);
         System.out.println("Result from input : " + userInput);
     }
 }
